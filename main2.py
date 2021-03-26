1bee06e4138293dd1a1b9a301851a5a03ef668ae
Doyle Luke <826884@stma.k12.mn.us>
2021-03-26 10:21:28 -0500

    practice

---
+++ main2.py
@@ -0,0 +1,13 @@
+light = input("Stoplight Color:\n")
+
+if light == "Green":
+    print("GO!")
+
+elif light == "Yellow":
+    print("SLOW!")
+
+elif light == "Blue":
+    print("CALL 911")
+
+else:
+    print("STOP!")
